09ac0cc25a2667df735b1b266c47cc5ce10753c2
Ubuntu <ubuntu@ip-172-31-28-1.ec2.internal>
2022-02-21 07:55:50 +0000

    proper code style

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Hertz Antonella All rights reserved.
+// 
+// Created by Hertz Antonella
+// Date: feb.21,2022
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+}     
